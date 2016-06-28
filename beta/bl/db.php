d60ce51f0ad5667ae117d7e091e173eb8d7d3635
rishabhsinghal2410 <rishabhsinghal2410@gmail.com>
2016-06-28 20:01:13 +0530

    clear db configuration

--- beta/bl/db.php
+++ beta/bl/db.php
@@ -1,9 +1,14 @@
 <?
 	/* Important COnfigurations to be made for database to function properly */
-	$databasehost = 'localhost'; //Host of the mySQL database 
-	$databaseuser = 'pikretmj_webuser';      //User name of the mySQL database. Its "root" by default 
-	$databasepassword = 'Tarac-603';      //Password for the mySQL databse access to the databaseuser  
-	$databasename = 'pikretmj_pikreview'; //Specify the database to use 
+	$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+	$cleardb_server = $cleardb_url["host"];
+	$cleardb_username = $cleardb_url["user"];
+	$cleardb_password = $cleardb_url["pass"];
+	$cleardb_db = substr($cleardb_url["path"],1);
+	$databasehost = $cleardb_server; //Host of the mySQL database 
+	$databaseuser = $cleardb_username;      //User name of the mySQL database. Its "root" by default 
+	$databasepassword = $cleardb_password;      //Password for the mySQL databse access to the databaseuser  
+	$databasename = $cleardb_db; //Specify the database to use 
 	function begin() {
 		@mysql_query("BEGIN");
 	}
